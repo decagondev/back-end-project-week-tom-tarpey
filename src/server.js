19e47ff9973576252ebcc1682c28593ffa09f30f
decagondev <decagondevelopment@gmail.com>
2018-10-29 16:57:21 +0000

    (MVP : BOILERPLATE : server.js) : use express.json()

--- src/server.js
+++ src/server.js
@@ -3,4 +3,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const mainRoutes = require("./api/mainRoutes");
 
+// instantiate the express server
 const server = express();
+
+// use directives
+server.use(express.json());
