6b1c67402d5796f9e20d854bfc100fa45cc136ea
decagondev <decagondevelopment@gmail.com>
2018-10-29 17:42:13 +0000

    (MVP : ROUTES) mainRoutes.js : put note at id (edit note) skeleton

--- src/api/mainRoutes.js
+++ src/api/mainRoutes.js
@@ -5,15 +5,22 @@ const session = require("express-session");
 
 const notes = null; // TODO: add linkup of helpers
 const users = null; // TODO: add linkup of helpers
+
 // get notes
 router.get("/notes", async (req, res) => {
   // TODO: add logic
 });
 
+// get note at id
 router.get("/notes/:id", async (req, res) => {
   // TODO: add logic
 });
 
+// post notes (create a new note)
 router.post("/notes", noteCheck, async (req, res) => {
+  // TODO: add logic
+});
+
+router.put("/notes/:id", noteCheck, async (req, res) => {
   // TODO: add logic
 });
