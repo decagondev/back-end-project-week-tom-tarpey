40625752a32caddb80afeae41e4b9b99f0e4df4a
decagondev <decagondevelopment@gmail.com>
2018-10-29 18:04:02 +0000

    (MVP : MODEL HELPERS) : get: method

--- data/models/notesModel.js
+++ data/models/notesModel.js
@@ -1,1 +1,19 @@
 const db = require("../dbConfig");
+
+module.exports = {
+  get: id => {
+    let query = db("notes");
+
+    if (id) {
+      return query.where("id", id).first();
+    }
+
+    return query;
+  },
+
+  insert: note => {
+    return db("notes")
+      .insert(note)
+      .then(([id]) => module.exports.get(id));
+  }
+};
