33b5ed17fae631cd6b6892731f33eaabc8b6ca41
decagondev <decagondevelopment@gmail.com>
2018-10-29 18:19:55 +0000

    (MVP : MODEL HELPERS) : insert: method (users)

--- data/models/usersModel.js
+++ data/models/usersModel.js
@@ -1,9 +1,14 @@
 const db = require("../dbConfig");
 
 module.exports = {
+  // get method
   get: credentials => {
     return db("users")
       .where({ username: credentials.username })
       .first();
+  },
+  // insert method
+  insert: user => {
+    return db("users").insert(user);
   }
 };
