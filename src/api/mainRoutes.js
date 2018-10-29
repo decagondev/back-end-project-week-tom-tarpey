f86ebeda3984a9b0ed4a1e234839cdadefafa586
decagondev <decagondevelopment@gmail.com>
2018-10-29 17:39:53 +0000

    (MVP : ROUTES) mainRoutes.js : post notes skeleton

--- src/api/mainRoutes.js
+++ src/api/mainRoutes.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
 
-const notes =
-  // get notes
-  router.get("/notes", async (req, res) => {
-    // TODO: add logic
-  });
+const notes = null; // TODO: add linkup of helpers
+const users = null; // TODO: add linkup of helpers
+// get notes
+router.get("/notes", async (req, res) => {
+  // TODO: add logic
+});
 
 router.get("/notes/:id", async (req, res) => {
+  // TODO: add logic
+});
+
+router.post("/notes", noteCheck, async (req, res) => {
   // TODO: add logic
 });
