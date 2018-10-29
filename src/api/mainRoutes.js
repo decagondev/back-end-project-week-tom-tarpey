2adee7d78523c39dda5f660ec878388a49d759c9
decagondev <decagondevelopment@gmail.com>
2018-10-29 18:48:01 +0000

    (MVP : ROUTES : LOGIC) : get logic

--- src/api/mainRoutes.js
+++ src/api/mainRoutes.js
@@ -8,7 +8,33 @@ const users = require("../../data/models/usersModel");
 
 // get notes
 router.get("/notes", async (req, res) => {
-  // TODO: add logic
+  try {
+    // get all notes
+    const allNotes = await notes.get().orderBy("id", "desc");
+
+    // setup the order string
+    const noteOrderString = await users.getNoteOrder(1);
+
+    // parse the json and extract the noteOrderArray
+    const noteOrderArray = JSON.parse(noteOrderString.noteOrder);
+    // check there is a note order array
+    if (noteOrderArray.length === 0) {
+      return res.status(200).json(allNotes);
+    } else {
+      // set an orderedNotes by mapping over the array and ordering via a find
+      const orderedNotes = noteOrderArray.map(order => {
+        return allNotes.find(note => note.id === order);
+      });
+      // return the ordered notes to the caller
+      return res.status(200).json(orderedNotes);
+    }
+  } catch (error) {
+    // cath any error left and send it to the caller
+    return res.status(500).json({
+      message: "the notes could not be retrieved",
+      error: error.message
+    });
+  }
 });
 
 // get note at id
