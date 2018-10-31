c7d769350e8a816083eaa40f412abce659fa09b8
decagondev <decagondevelopment@gmail.com>
2018-10-31 17:38:09 +0000

    (STRETCH) : DB : updated config for production

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,12 @@
+require("dotenv").config();
+const localPg = {
+  host: "localhost",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -10,6 +19,22 @@ module.exports = {
     },
     seeds: {
       directory: "./data/seeds"
+    }
+  },
+  //add config
+  production: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds"
     }
   }
 };
